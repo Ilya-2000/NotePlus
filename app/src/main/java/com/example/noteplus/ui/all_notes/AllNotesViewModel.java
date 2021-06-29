1433c76fe8b84bb4bb7857d0d25924fd7ac59199
Ilya Ponomaryov <ip287733@gmail.com>
2021-06-29 23:28:34 +0500

    wsw

--- app/src/main/java/com/example/noteplus/ui/all_notes/AllNotesViewModel.java
+++ app/src/main/java/com/example/noteplus/ui/all_notes/AllNotesViewModel.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.example.noteplus.data.db.NoteDao;
 import com.example.noteplus.data.db.NoteRoomDb;
@@ -15,19 +14,38 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+
+
 public class AllNotesViewModel extends AndroidViewModel {
     private NoteDao noteDao;
     private LiveData<List<Note>> noteList;
+    private CompositeDisposable compositeDisposable;
 
     public AllNotesViewModel(@NonNull @NotNull Application application) {
         super(application);
         NoteRoomDb noteDb = NoteRoomDb.getDatabase(application);
         noteDao = noteDb.noteDao();
-        //noteList = noteDao.getAllNotes();
+       /* compositeDisposable(noteDao.getAllNotes()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(
+                {
+                        setAllNotes();
+                }
+        ));*/
+
+
     }
 
-    public LiveData<List<Note>> getAllNotes() {
+   /* public LiveData<List<Note>> getAllNotes() {
         return noteList;
+    }*/
+
+    public void setAllNotes(LiveData<List<Note>> notes) {
+        noteList = notes;
     }
 
     public void deleteNote(Note note) {
