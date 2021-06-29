72543966f0fb2adf8bfeca4237f15556343d0508
Ilya-2000 <ip287733@gmail.com>
2021-06-29 16:36:20 +0500

    vm

--- app/src/main/java/com/example/noteplus/data/db/NoteDao.java
+++ app/src/main/java/com/example/noteplus/data/db/NoteDao.java
@@ -5,22 +5,28 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.example.noteplus.models.Note;
 
 import java.util.List;
+
+import io.reactivex.Observable;
 
 import static androidx.room.OnConflictStrategy.REPLACE;
 
 @Dao
 public interface NoteDao {
     @Query("SELECT * from Note")
-    LiveData<List<Note>> getAllNotes();
+    Observable<LiveData<List<Note>>> getAllNotes();
 
     @Insert(onConflict = REPLACE)
     void addNote(Note note);
 
     @Delete
     void deleteNote(Note note);
+
+    @Update
+    void updateNote(Note note);
 
 }
--- app/src/main/java/com/example/noteplus/ui/all_notes/AllNotesViewModel.java
+++ app/src/main/java/com/example/noteplus/ui/all_notes/AllNotesViewModel.java
@@ -1,6 +1,36 @@
 package com.example.noteplus.ui.all_notes;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-public class AllNotesViewModel extends ViewModel {
+import com.example.noteplus.data.db.NoteDao;
+import com.example.noteplus.data.db.NoteRoomDb;
+import com.example.noteplus.models.Note;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class AllNotesViewModel extends AndroidViewModel {
+    private NoteDao noteDao;
+    private LiveData<List<Note>> noteList;
+
+    public AllNotesViewModel(@NonNull @NotNull Application application) {
+        super(application);
+        NoteRoomDb noteDb = NoteRoomDb.getDatabase(application);
+        noteDao = noteDb.noteDao();
+        //noteList = noteDao.getAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return noteList;
+    }
+
+    public void deleteNote(Note note) {
+        noteDao.deleteNote(note);
+    }
 }
